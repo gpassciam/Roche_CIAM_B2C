2e15c3f14c505ff1467a35e169f7becff725f957
ciampoc <104811739+gpassciam@users.noreply.github.com>
2022-08-26 00:51:36 +0530

    up

--- assets/js/IndexedDB.js
+++ assets/js/IndexedDB.js
@@ -6,13 +6,13 @@
             return;
         }
         else {
-            
+
             console.log("Your browser support IndexedDB");
-            
+
 
             //alert(Key);
             // open the CRM database with the version 1
-            var request =  indexedDB.open(DBName, 3);
+            var request = indexedDB.open(DBName, 3);
 
             // create the Contacts object store and indexes
             request.onupgradeneeded = (event) => {
@@ -40,43 +40,43 @@
 
             // handle the success event
             request.onsuccess = (event) => {
-                
+
                 console.log("On_ProcessDB_Onsuccess");
-                
+
                 insert(event, Table, Data);
             }
 
 
         }
 
-      
+
     });
 }
 
 
-function insert(event,Table,Data) {
+function insert(event, Table, Data) {
 
     var db = event.target.result;
-	var txn = event.target.transaction;
-	let store;
+    var txn = event.target.transaction;
+    let store;
     //var txn;
     //const store = txn.objectStore(Table);
-      //else {
-        //console.log("On_Insert_Store_Exists");
-        txn = db.transaction(Table, 'readwrite');
-		store = txn.objectStore(Table);
-		
-	//}
-        let query = store.put(Data);
-           query.onsuccess = function (event) {
-            console.log("On_Insert_Success");
-        };
+    //else {
+    //console.log("On_Insert_Store_Exists");
+    txn = db.transaction(Table, 'readwrite');
+    store = txn.objectStore(Table);
+
+    //}
+    let query = store.put(Data);
+    query.onsuccess = function (event) {
+        console.log("On_Insert_Success");
+    };
     ''
-        // handle the error case
-        query.onerror = function (event) {
-            
-            console.log("On_Insert_Error : " + event.target.errorCode);
-        };
+    // handle the error case
+    query.onerror = function (event) {
+
+        console.log("On_Insert_Error : " + event.target.errorCode);
+    };
 
     // close the database once the 
     // transaction completes
@@ -96,14 +96,14 @@ function getUID(DBName, Table, UID) {
         request.onsuccess = (event) => {
 
             console.log("On_getUID_onsuccess");
-            getUIDChild(event, Table, UID,resolve);
+            getUIDChild(event, Table, UID, resolve);
 
         };
 
         request.onupgradeneeded = (event) => {
 
             console.log("On_getUID_onupgrade");
-            getUIDChild(event, Table, UID,resolve);
+            getUIDChild(event, Table, UID, resolve);
         }
 
         request.onerror = (event) => {
@@ -122,7 +122,7 @@ function getUIDChild(event, Table, UID, resolve) {
     let db = event.target.result;
     var SOrgName;
     var Orgid;
-	//var txn = event.target.transaction;
+    //var txn = event.target.transaction;
     let txn = db.transaction(Table, 'readwrite');
     let store = txn.objectStore(Table);
     const index = store.index('UID');
@@ -136,7 +136,26 @@ function getUIDChild(event, Table, UID, resolve) {
         //SName = query.result.profile.firstName + " " + query.result.profile.lastName;
         SName = query.result.profile.firstName;
         Sprovider = query.result.provider;
-       
+
+        if (sessionStorage.getItem("flag") == 'b2b') {
+            SOrgName = query.result.groups.organizations[0].orgName;
+            SUID = SUID + "," + SName + "," + Sprovider + "," + SOrgName;
+            Orgid = query.result.groups.organizations[0].orgId;
+            var roles = query.result.groups.organizations[0].roles;
+            sessionStorage.setItem("OrgID", Orgid);
+            sessionStorage.setItem("roles", roles);
+            roles.forEach(function (roleid) {
+                role = localStorage.getItem(roleid);
+                if (role == "Delegated Admin") {
+                    sessionStorage.setItem("DACheck", "Yes");
+
+                }
+
+            }
+            )
+        }
+
+        else {
             //SOrgName = query.result.groups.organizations[0].orgName;
             SUID = SUID + "," + SName + "," + Sprovider;
             //alert(SUID);
@@ -155,29 +174,29 @@ function getUIDChild(event, Table, UID, resolve) {
         db.close();
         //return SUID;
     }
-
+}
 
 function UpdateData(DBName, Table, Data, UID) {
     var request = indexedDB.open(DBName);
-        // handle the success event
+    // handle the success event
     request.onsuccess = (event) => {
         console.log("On_UpdateData_onsuccess");
-            UpdateDataChild(event, Table, Data);
-        }
-        request.onerror = (event) => {
-            console.log("On_UpdateData_onerror : " + event.target.errorCode);
-        };
+        UpdateDataChild(event, Table, Data);
+    }
+    request.onerror = (event) => {
+        console.log("On_UpdateData_onerror : " + event.target.errorCode);
+    };
     request.onupgradeneeded = (event) => {
 
         console.log("On_UpdateData_onupgrade");
         UpdateDataChild(event, Table, Data);
     }
 }
-            
-function UpdateDataChild(event,Table, Data) {
+
+function UpdateDataChild(event, Table, Data) {
 
     let db = event.target.result;
-	//var txn = event.target.transaction;
+    //var txn = event.target.transaction;
     var transaction = db.transaction(Table, "readwrite");
     var store = transaction.objectStore(Table);
     //alert("ni" + Data);
@@ -208,7 +227,7 @@ function UpdateDataChild(event,Table, Data) {
 
 
 function Delete(DBName, Table, UID) {
-    
+
 
     return new Promise(function (resolve) {
         var request = indexedDB.open(DBName);
@@ -229,25 +248,25 @@ function Delete(DBName, Table, UID) {
     });
 }
 
-function DeleteChild(event,UID) {
+function DeleteChild(event, UID) {
 
 
     let db = event.target.result;
-	//var txn = event.target.transaction;
+    //var txn = event.target.transaction;
     txn = db.transaction(Table, "readwrite");
     objectStore = txn.objectStore(Table);
     request = objectStore.delete(UID);
     console.log("deleted UID : " + UID);
     request.onsuccess = function (evt) {
         console.log("On_DeleteChild_onsuccess");
-        
+
     };
 
     request.onerror = function (evt) {
         console.log("On_DeleteChild_onerror : " + event.target.errorCode);
 
     };
-	// close the database connection
+    // close the database connection
     txn.oncomplete = function () {
         db.close();
     };
@@ -255,36 +274,36 @@ function DeleteChild(event,UID) {
 }
 
 function getRecords(DBName, Table, UID) {
- 
+
     const request = indexedDB.open(DBName);
 
     // handle the success event
     request.onsuccess = (event) => {
         console.log("On_getRecords_onsuccess");
-        getRecorsChild(event,Table, UID);
+        getRecorsChild(event, Table, UID);
 
     };
 
     request.onupgradeneeded = (event) => {
 
         console.log("On_getRecords_onupgrade");
-        getRecorsChild(event,Table, UID);
+        getRecorsChild(event, Table, UID);
 
     }
 
     request.onerror = (event) => {
 
         console.log("On_getRecords_onerror");
-        
+
 
     }
 
 }
 
-function getRecorsChild(event,Table,UID) {
+function getRecorsChild(event, Table, UID) {
 
     let db = event.target.result;
-	//var txn= event.target.transaction;
+    //var txn= event.target.transaction;
     var txn = db.transaction(Table, 'readonly');
     var store = txn.objectStore(Table);
     const index = store.index('UID');
@@ -297,8 +316,8 @@ function getRecorsChild(event,Table,UID) {
         console.log("On_getRecordsChild_onsuccess");
         var result = event.target.result;
         console.log(result);
-        
-            //document.getElementById('UID').value = result.UID;
+
+        //document.getElementById('UID').value = result.UID;
         document.getElementById('photo').src = result.profile.photoURL;
         if (typeof result.profile.email == "undefined") {
 
@@ -322,7 +341,7 @@ function getRecorsChild(event,Table,UID) {
         else {
             document.getElementById('lastName').innerHTML = result.profile.lastName;
         }
-            
+
         if (typeof result.profile.address == "undefined") {
             document.getElementById('address').innerHTML = " ";
         }
@@ -354,7 +373,7 @@ function getRecorsChild(event,Table,UID) {
         else {
             document.getElementById('country').innerHTML = result.profile.country;
         }
-            
+
 
         if (sessionStorage.getItem("flag") == 'b2c') {
             if (typeof result.data.PhoneNumber == "undefined") {
@@ -366,13 +385,79 @@ function getRecorsChild(event,Table,UID) {
                 document.getElementById('phone').innerHTML = result.data.PhoneNumber;
             }
 
-            
+            document.getElementById('Organization').innerHTML = " ";
+            document.getElementById('Department').innerHTML = " ";
+            document.getElementById('Job').innerHTML = " ";
+            document.getElementById('Roles').innerHTML = " ";
         }
-        
-          
-       
-            console.dir(result);
-        
+
+        //alert(result.groups.organizations[0].orgName);
+
+        var role;
+        var rolenames = '';
+        if (sessionStorage.getItem("flag") == 'b2b') {
+            //document.getElementById('phone').innerHTML = result.identities.phones.number;
+            //document.getElementById('email').innerHTML = result.profile.email;
+            //document.getElementById('firstName').innerHTML = result.profile.firstName;
+            //document.getElementById('lastName').innerHTML = result.profile.lastName;
+            //document.getElementById('address').innerHTML = result.profile.address;
+            //document.getElementById('city').innerHTML = result.profile.city;
+            //document.getElementById('state').innerHTML = result.profile.state;
+            //document.getElementById('zip').innerHTML = result.profile.zip;
+
+            //document.getElementById('country').innerHTML = result.profile.country;
+            if (typeof result.phoneNumber == "undefined") {
+                document.getElementById('phone').innerHTML = " ";
+            }
+            else {
+
+                document.getElementById('phone').innerHTML = result.phoneNumber;
+            }
+
+            if (typeof result.groups.organizations[0].orgName == "undefined") {
+                document.getElementById('Organization').innerHTML = " ";
+            }
+            else {
+                document.getElementById('Organization').innerHTML = result.groups.organizations[0].orgName;
+            }
+
+            if (typeof result.groups.organizations[0].department == "undefined") {
+                document.getElementById('Department').innerHTML = " ";
+            }
+            else {
+                document.getElementById('Department').innerHTML = result.groups.organizations[0].department;
+            }
+            if (typeof result.groups.organizations[0].job == "undefined") {
+                document.getElementById('Job').innerHTML = " ";
+            }
+            else {
+                document.getElementById('Job').innerHTML = result.groups.organizations[0].job;
+            }
+
+            var roles = result.groups.organizations[0].roles;
+            var i = roles.length;
+            //alert(i);
+            roles.forEach(function (roleid) {
+
+                role = localStorage.getItem(roleid);
+                //if (i > 1) {
+                //    role = role + ";";
+                //}
+                //else {
+                //    role = role;
+                //}
+                role = role + "<br\>";
+                rolenames = rolenames + role;
+            });
+
+            document.getElementById('Roles').innerHTML = rolenames;
+
+
+
+            //document.getElementById('Roles').value = result.groups.organizations[0].roles;
+        }
+        console.dir(result);
+
     };
 
     query.onerror = (event) => {
@@ -397,10 +482,10 @@ function getRecorsChild(event,Table,UID) {
 
 
 
-        
+
 //(function () {
 
-    
+
 
 //    // open the CRM database with the version 1
 //    const request = indexedDB.open('UserInfo', 1);
